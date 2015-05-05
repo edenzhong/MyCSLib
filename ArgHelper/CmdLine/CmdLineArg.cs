5b2e7452d738c8e1557e8b9ba277b1f870c584b1
Eden Zhong <Eden.zhong@gmail.com>
2015-05-05 10:24:31 +0800

    cmdline loader done.

--- ArgHelper/CmdLine/CmdLineArg.cs
+++ ArgHelper/CmdLine/CmdLineArg.cs
@@ -6,6 +6,47 @@ using System.Threading.Tasks;
 
 namespace ArgHelper.CmdLine
 {
+    public class CmdArgs
+    {
+        public Dictionary<string, object> ArgPairs = new Dictionary<string, object>();
+        public List<string> Args = new List<string>();
+
+        public void Add(KeyValuePair<string, string> kv)
+        {
+            if (null != kv.Key)
+            {
+                if (ArgPairs.Keys.Contains(kv.Key))
+                {
+                    object o = ArgPairs[kv.Key];
+                    if (o is string)
+                    {
+                        List<string> l = new List<string>();
+                        l.Add(o as string);
+                        l.Add(kv.Value);
+                        ArgPairs[kv.Key] = l;
+                    }
+                    else if (o is List<string>)
+                    {
+                        List<string> l = o as List<string>;
+                        l.Add(kv.Value);
+                    }
+                    else
+                    {
+                        // ??
+                        throw new Exception("Not implement");
+                    }
+                }
+                else
+                {
+                    ArgPairs.Add(kv.Key, kv.Value);
+                }
+            }
+            else
+            {
+                Args.Add(kv.Value);
+            }
+        }
+    }
     public class CmdLineArg
     {
         public static CmdArgs Parse(string[] args)
@@ -32,52 +73,62 @@ namespace ArgHelper.CmdLine
                 {
                     carg.Add(kv);
                 }
+                else
+                {
+                    return null;
+                }
                 i += cover;
             }
             return carg;
         }
-        
     }
 
-    public class CmdArgs
-    {
-        public bool Inited { get; set; }
-        public Dictionary<string, object> ArgPairs = new Dictionary<string, object>();
-        public List<string> Args = new List<string>();
+    
 
-        public void Add(KeyValuePair<string, string> kv)
+
+    public static class CmdLineArgLoader
+    {
+        public static bool LoadCmdLine(this IArg arg, string[] cmdLineArgs)
         {
-            if ( null != kv.Key)
+            if (null == arg)
             {
-                if ( ArgPairs.Keys.Contains(kv.Key))
+                return false;
+            }
+            CmdArgs commandArg = CmdLineArg.Parse(cmdLineArgs);
+            if (null == commandArg)
+            {
+                return false;
+            }
+
+            Dictionary<string, object> argPairs = commandArg.ArgPairs;
+            List<string> keys = argPairs.Keys.ToList();
+
+
+            bool state = arg.LoadArg(
+                (key, defVal) =>
                 {
-                    object o = ArgPairs[kv.Key];
-                    if ( o is string)
+                    if (keys.Contains(key))
                     {
-                        List<string> l = new List<string>();
-                        l.Add(o as string);
-                        l.Add(kv.Value);
-                        ArgPairs[kv.Key] = l;
-                    }
-                    else if ( o is List<string>)
-                    {
-                        List<string> l = o as List<string>;
-                        l.Add(kv.Value);
+                        keys.Remove(key);
+                        return argPairs[key] as string;
                     }
                     else
                     {
-                        // ??
-                        throw new Exception("Not implement");
+                        return defVal;
                     }
                 }
-                else
+                , (key) =>
                 {
-                    ArgPairs.Add(kv.Key, kv.Value);
+                    return commandArg.Args.Contains(key);
                 }
+            );
+            if (keys.Count > 0) // there are some invalid command line arguments.
+            {
+                return false;
             }
             else
             {
-                Args.Add(kv.Value);
+                return state;
             }
         }
     }
---
+++ ArgHelperTests/CmdLine/CmdLineArgLoaderTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArgHelper.CmdLine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ArgHelper.CmdLine.Tests
+{
+    [TestClass()]
+    public class CmdLineArgLoaderTests
+    {
+        class MyCmdLineArg:IArg
+        {
+            [ArgInf]
+            public string abc{get;set;}
+            [ArgInf]
+            public int intVal{get;set;}
+            [ArgInf]
+            public string equalVal{get;set;}
+
+            [ArgInf("","",ArgInfAttribute.ArgType.StandAlone)]
+            public bool standaloneVal{get;set;}
+
+            public bool ArgInited { get; set; }
+        }
+
+        [TestMethod()]
+        public void LoadCmdLineTest()
+        {
+            string[] args = {"-abc","abcval","--intVal","1","standaloneVal","equalVal=Equal-value"};
+
+            MyCmdLineArg myarg = new MyCmdLineArg();
+            myarg.LoadCmdLine(args);
+            
+        }
+    }
+}
